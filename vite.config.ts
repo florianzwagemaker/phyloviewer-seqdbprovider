e978395e13dd1248c2ba30eda151ba5d9d8185a7
Florian Zwagemaker <f.zwagemaker@gmail.com>
2025-09-30 16:34:35 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/phyloviewer-wasm/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/phyloviewer-seqdbprovider/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
